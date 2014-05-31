69a83bcdf7862a973683b756d1146ddada1135a6
Patrick Shan <patrick@freelancer.com>
2014-05-31 21:27:59 +1000

    add backup steps for install script

--- install.sh
+++ install.sh
@@ -3,8 +3,24 @@
 script_name=`readlink -f ${0}`
 script_path=`dirname ${script_name}`
 
+run_time=`date +%Y%m%d%H%M`
+
+if [ -e ${HOME}/.dircolors ]
+then
+    mv -v ${HOME}/.dircolors ${HOME}/.dircolors.bak.${run_time}
+fi
 ln -s `readlink -f ${script_path}/dircolors` ${HOME}/.dircolors
+
+if [ -e ${HOME}/.vimrc ]
+then
+    mv -v ${HOME}/.vimrc ${HOME}/.vimrc.bak.${run_time}
+fi
 ln -s `readlink -f ${script_path}/vimrc` ${HOME}/.vimrc
+
+if [ -e ${HOME}/.vim ] 
+then
+    mv -v ${HOME}/.vim ${HOME}/.vim.bak.${run_time}
+fi
 ln -s `readlink -f ${script_path}/vim` ${HOME}/.vim
 
 echo "> Please change console profile to use solarized scheme <"
