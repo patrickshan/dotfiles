df12e98470ee637005b1aaa2e54549a1ad8044d9
Patrick Shan <patrickshan.syd@gmail.com>
2018-07-28 16:24:56 +1000

    add a new command to fix intellisense

--- gotools.sh
+++ gotools.sh
@@ -3,3 +3,9 @@
 go get github.com/uber/go-torch
 
 go get -u github.com/kisielk/errcheck
+
+go get -u -v github.com/gobuffalo/buffalo/buffalo
+
+# re-enable intellisense for vscode after go upgrade
+gocode close
+go get -u github.com/nsf/gocode
